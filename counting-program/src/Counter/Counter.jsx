6cc001c16c4577c7dc84c61605fbe9797a76fa2c
= <seanparsons2003@gmail.com>
2025-05-06 16:07:59 +0100

    Creates 3 buttons, some imports, displays buttons on app

---
+++ counting-program/src/Counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+function Button() {
+    return(
+        <div>
+            <button>Decrement</button>
+            <button>Reset</button>
+            <button>Increment</button>
+        </div>
+    );
+}
+
+export default Button
