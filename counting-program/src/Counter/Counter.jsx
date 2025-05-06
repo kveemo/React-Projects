79f177bf39f6d29d9f5e5956124070e0a1cfa46e
= <seanparsons2003@gmail.com>
2025-05-06 16:36:54 +0100

    Added counter functions and added them to buttons, buttons now increment, decrement and reset the value

--- counting-program/src/Counter/Counter.jsx
+++ counting-program/src/Counter/Counter.jsx
@@ -1,11 +1,38 @@
 import React, {useState} from "react";
 import styles from './Counter.module.css';
+
 function Button() {
+
+    const [value, setValue] = useState(0);
+
+    const decrementValue = () => {
+        setValue(value-1);
+    }
+
+    const resetValue = () => {
+        setValue(0);
+    }
+
+    const incrementValue = () => {
+        setValue(value+1);
+    }
+    
     return(
-        <div className={styles.buttonContainer}>
-            <button className = {styles.decrementButton}>Decrement</button>
-            <button className = {styles.resetButton}>Reset</button>
-            <button className = {styles.incrementButton}>Increment</button>
+        <div>
+            <p>
+                {value}
+            </p>
+            <div className={styles.buttonContainer}>
+
+                <button onClick = {decrementValue} 
+                    className = {styles.decrementButton}>Decrement</button>
+
+                <button onClick = {resetValue} 
+                    className = {styles.resetButton}>Reset</button>
+
+                <button onClick = {incrementValue} 
+                    className = {styles.incrementButton}>Increment</button>
+            </div>
         </div>
     );
 }
